441b56028f81d928f84ca4082e44fc1fd24741cb
David Moreno Capel <morenocapeldavid@gmail.com>
2020-06-06 19:28:44 +0200

    Generado el header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,44 +1,63 @@
+import * as $ from 'jquery';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { routing, appRoutingProviders } from './app.routing';
+import { MomentModule } from 'angular2-moment';
 
-import { AppRoutingModule } from './app-routing.module';
+import { HttpModule } from '@angular/http';
+
+// Module Custom
+import { MessagesModule} from './messages/messages.module';
+
+// Components
 import { AppComponent } from './app.component';
-import { FooterComponent } from './component/footer/footer.component';
-import { HeaderComponent } from './component/header/header.component';
-import { FollowedComponent } from './component/followed/followed.component';
-import { FollowingComponent } from './component/following/following.component';
-import { HomeComponent } from './component/home/home.component';
-import { LoginComponent } from './component/login/login.component';
-import { ProfileComponent } from './component/profile/profile.component';
-import { PublicationsComponent } from './component/publications/publications.component';
-import { RegisterComponent } from './component/register/register.component';
-import { SidebarComponent } from './component/sidebar/sidebar.component';
-import { TimelineComponent } from './component/timeline/timeline.component';
-import { UserEditComponent } from './component/user-edit/user-edit.component';
-import { UsersComponent } from './component/users/users.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { UserEditComponent } from './components/user-edit/user-edit.component';
+import { UsersComponent } from './components/users/users.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { TimelineComponent } from './components/timeline/timeline.component';
+import { PublicationsComponent } from './components/publications/publications.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { FollowingComponent } from './components/following/following.component';
+import { FollowedComponent } from './components/followed/followed.component';
+
+// Services
+import {UserService} from './services/user.service';
+import {UserGuard} from './services/user.guard';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FooterComponent,
-    HeaderComponent,
-    FollowedComponent,
-    FollowingComponent,
-    HomeComponent,
     LoginComponent,
-    ProfileComponent,
-    PublicationsComponent,
     RegisterComponent,
+    HomeComponent,
+    UserEditComponent,
+    UsersComponent,
     SidebarComponent,
     TimelineComponent,
-    UserEditComponent,
-    UsersComponent
+    PublicationsComponent,
+    ProfileComponent,
+    FollowingComponent,
+    FollowedComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    routing,
+    HttpClientModule,
+    MomentModule,
+    MessagesModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [
+      appRoutingProviders,
+      UserService,
+      UserGuard
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
