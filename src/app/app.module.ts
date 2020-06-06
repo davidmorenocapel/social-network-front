733078e7505cb5e75ce3297a6bfab8ee300b0288
David Moreno Capel <morenocapeldavid@gmail.com>
2020-06-06 19:11:33 +0200

    User-edt created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { PublicationsComponent } from './component/publications/publications.com
 import { RegisterComponent } from './component/register/register.component';
 import { SidebarComponent } from './component/sidebar/sidebar.component';
 import { TimelineComponent } from './component/timeline/timeline.component';
+import { UserEditComponent } from './component/user-edit/user-edit.component';
 
 @NgModule({
   declarations: [
@@ -28,7 +29,8 @@ import { TimelineComponent } from './component/timeline/timeline.component';
     PublicationsComponent,
     RegisterComponent,
     SidebarComponent,
-    TimelineComponent
+    TimelineComponent,
+    UserEditComponent
   ],
   imports: [
     BrowserModule,
