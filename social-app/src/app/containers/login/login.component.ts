4da7d0251a7fb794bee58cb28bf2f63e1a6d1e8b
David Moreno Capel <morenocapeldavid@gmail.com>
2020-06-07 18:38:22 +0200

    servicio user

--- social-app/src/app/containers/login/login.component.ts
+++ social-app/src/app/containers/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Validators, FormGroup, FormControl, FormControlName} from '@angular/forms';
+import { ApiService } from './../../services/user/user.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.sass']
 })
 export class LoginComponent implements OnInit {
+  error = '';
+  success = '';
+  userForm = new FormGroup({
+    email: new FormControl('', Validators.required),
+    password: new FormControl('')
+  });
 
-  constructor() { }
+  constructor(
+    //private userService: UserService
+  ) { }
 
   ngOnInit(): void {
+
+    
+  click_login() {
+    console.log(this.userForm.value);
+
+    this.error = '';
+    this.success = '';
+    this.apiservice.login(this.userForm.value).then(
+      res => {
+        this.success = res;
+        console.log("hola");
+      },
+      (err) => {
+        this.error = err;
+      }
+    );
+  }
+
+  validarFormular() {}
+
+  showPass() {
+    const x = (document.getElementById('exampleInputPassword1') as HTMLInputElement);
+    if (x.type === 'password') {
+      x.type = 'text';
+    } else {
+      x.type = 'password';
+    }
+
   }
 
 }
