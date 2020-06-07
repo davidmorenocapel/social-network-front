cc7be605425af9a394ce4c2e469ee7fdb7d58483
David Moreno Capel <morenocapeldavid@gmail.com>
2020-06-07 17:37:19 +0200

    Generando componentes y contenedores

--- social-app/src/app/app.module.ts
+++ social-app/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './containers/register/register.component';
+import { LoginComponent } from './containers/login/login.component';
+import { UserInfoComponent } from './containers/user-info/user-info.component';
+import { TimelineComponent } from './containers/timeline/timeline.component';
+import { PostComponent } from './containers/post/post.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    UserInfoComponent,
+    TimelineComponent,
+    PostComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
